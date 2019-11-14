26e616d9cbc235d60623dca35b1a301a0bdb79b4
Bryan Snyder <snyderb.de@gmail.com>
2019-11-14 18:22:37 -0500

    added map-styles, deck add ons, imported deck modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
+
+import { GoogleMapsOverlay } from '@deck.gl/google-maps';
+import { HexagonLayer, HeatmapLayer } from '@deck.gl/aggregation-layers';
+import { ScatterplotLayer } from '@deck.gl/layers';
+import mapStyles from './map-styles';
+
 // call the initMap function when Google Maps is loaded
 
 window.initMap = () => {
